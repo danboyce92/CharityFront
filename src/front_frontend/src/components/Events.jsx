a02471ac38e68cdc2e63e8a755494f9ae77733d4
Daniel B <daarf92@gmail.com>
2024-02-24 08:20:35 +0000

    events page continued

--- src/front_frontend/src/components/Events.jsx
+++ src/front_frontend/src/components/Events.jsx
@@ -2,10 +2,53 @@ import React from 'react'
 
 const Events = () => {
   return (
-    <div>
-      <div>Header</div>
-      <div>Votes</div>
-      <div>Selection</div>
+    <div id="events">
+      <div id="container">
+        <div id="header">
+          <div id="balance">Current Balance</div>
+          <div id="deadline">Deadline</div>
+          <div id="deposit">Deposit</div>
+        </div>
+        <div id="votes">
+        Votes
+          <div id="vote-cont">
+            <div>Charity A - %</div>
+            <div>Charity B - %</div>
+            <div>Charity C - %</div>
+            <div>Charity D - %</div>
+          </div>
+        </div>
+        <div id="selection">
+          <div id="vote-a">
+            <div className='vote-card'>
+              <div>Charity A</div>
+              <div>Description & link to website</div>
+              <div>Button</div>
+            </div>
+          </div>
+          <div id="vote-b">
+            <div className='vote-card'>
+                <div>Charity B</div>
+                <div>Description & link to website</div>
+                <div>Button</div>
+              </div>
+          </div>
+          <div id="vote-c">
+            <div className='vote-card'>
+              <div>Charity C</div>
+              <div>Description & link to website</div>
+              <div>Button</div>
+            </div>
+          </div>
+          <div id="vote-d">
+            <div className='vote-card'>
+              <div>Charity D</div>
+              <div>Description & link to website</div>
+              <div>Button</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
